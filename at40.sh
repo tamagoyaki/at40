187be8083ab084cfdc339676087a3f0111214070
tamagoyaki <tamagoyaki@users.noreply.github.com>
2015-04-21 16:38:14 +0900

    first commit

---
+++ at40.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+
+IFS=$','
+CHS="quit,at40,70's"
+
+select ch in $CHS; do
+	if [ $ch = "quit" ]; then
+	    exit
+	elif [ $ch = "at40" ]; then
+	    mplayer -loop 0 -playlist http://americantop40.akacast.akamaistream.net/7/785/58084/v1/auth.akacast.akamaistream.net/americantop40.m3u
+	elif [ $ch = "70's" ]; then
+	    mplayer -loop 0 http://at70-fl.ng.akacast.akamaistream.net:80/7/763/234624/v1/auth.akacast.akamaistream.net/at70-fl
+	fi
+done
