8bd08ade710dedc79a4bb956ec5c1d5c83486990
tamagoyaki <tamagoyaki@users.noreply.github.com>
2015-04-22 11:16:55 +0900

    bug fix

--- at40.sh
+++ at40.sh
@@ -2,14 +2,14 @@
 #
 
 IFS=$','
-CHS="quit,at40,70's"
+CHS="quit,at40,7080"
 
 select ch in $CHS; do
-	if [ $ch = "quit" ]; then
+	if [ "$ch" = "quit" ]; then
 	    exit
-	elif [ $ch = "at40" ]; then
+	elif [ "$ch" = "at40" ]; then
 	    mplayer -loop 0 -playlist http://americantop40.akacast.akamaistream.net/7/785/58084/v1/auth.akacast.akamaistream.net/americantop40.m3u
-	elif [ $ch = "70's" ]; then
+	elif [ "$ch" = "7080" ]; then
 	    mplayer -loop 0 http://at70-fl.ng.akacast.akamaistream.net:80/7/763/234624/v1/auth.akacast.akamaistream.net/at70-fl
 	fi
 done
