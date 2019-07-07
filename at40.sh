b55b5e8f40dd85f75a0776215939d615482cf0d7
siu <siu@amakusa>
2019-07-07 11:56:30 +0900

    new url

--- at40.sh
+++ at40.sh
@@ -8,8 +8,8 @@ select ch in $CHS; do
 	if [ "$ch" = "quit" ]; then
 	    exit
 	elif [ "$ch" = "at40" ]; then
-	    mplayer -loop 0 -playlist http://americantop40.akacast.akamaistream.net/7/785/58084/v1/auth.akacast.akamaistream.net/americantop40.m3u
+	    mplayer -loop 0 -playlist http://c2.prod.playlists.ihrhls.com/4802/playlist.m3u8
 	elif [ "$ch" = "classic" ]; then
-	    mplayer -loop 0 http://at70-fl.ng.akacast.akamaistream.net:80/7/763/234624/v1/auth.akacast.akamaistream.net/at70-fl
+	    mplayer -loop 0 -playlist http://c9.prod.playlists.ihrhls.com/6545/playlist.m3u8
 	fi
 done
